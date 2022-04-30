2c3f5b2d2c62e05591ce5578eb99187cee5f1578
anavp <anavprasadanav@gmail.com>
2022-04-30 19:46:47 -0400

    debugging

--- read_write_test.cpp
+++ read_write_test.cpp
@@ -7,6 +7,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#define print_var(x) cout << #x << ": " << x << endl;
 
 using namespace std;
 
@@ -14,6 +15,8 @@ char * read_input_image(string path, string destination_path){
     const char *filename = path.c_str();
     int x, y, n = 4;
     unsigned char *data = stbi_load(filename, &x, &y, &n, 3);
+    print_var(x);
+    print_var(y);
     for (int i = 0; i < y; ++i){
         for (int j = 0; j < x; ++j){
             cout << data[i * y + j] << " ";
